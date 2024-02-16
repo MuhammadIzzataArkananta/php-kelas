ff6d4bc9baecafea1b591c213b3c2d09b0a01d04
MuhammadIzzataArkananta <bankfree426@gmail.com>
2024-02-16 08:59:21 +0700

    pbo php
    tanggal 16 februari 2024

---
+++ pbo/database.php
@@ -0,0 +1,36 @@
+<?php
+
+class Database{
+    public $db="dbtoko";
+    private $host="localhost";
+    private $user="root";
+    private $password="";
+    public function insertData() {
+        return "INSERT DATA";
+    }
+    public static function selectData() {
+        return "SELECT DATA";
+    }
+    public function deleteData() {
+        return "DELETE DATA";
+    }
+    public function updateData() {
+        return "UPDATE DATA";
+    }
+    public function ambil() {
+        return $this->host;
+    }
+}
+
+class penjualan {
+    public function barang() {
+        return "barang";
+    }
+    public static function pelanggan() {
+        return "pelanggan";
+    }
+}
+
+
+
+?>
